d8fd5b05e661d091eda70f25e8bcaa8e8683ac6a
Andrea Saderi <andrea.saderi16@gmail.com>
2026-07-28 16:35:49 +0200

    Creates auth and user controls/services

---
+++ src/main/java/andreasaderi/capstone/services/UserService.java
@@ -0,0 +1,63 @@
+package andreasaderi.capstone.services;
+
+import andreasaderi.capstone.entities.User;
+import andreasaderi.capstone.exceptions.FileNotAllowedException;
+import andreasaderi.capstone.exceptions.RecordAlreadyExistsException;
+import andreasaderi.capstone.repositories.UserRepository;
+import andreasaderi.capstone.requestDTOs.UserDTO;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final Cloudinary fileUploader;
+    private final PasswordEncoder bcrypt;
+
+    public UserService(UserRepository userRepository, Cloudinary fileUploader, PasswordEncoder bcrypt) {
+        this.userRepository = userRepository;
+        this.fileUploader = fileUploader;
+        this.bcrypt = bcrypt;
+    }
+
+    public User register(UserDTO body, MultipartFile profileImage) {
+        if (userRepository.existsByEmail(body.email()))
+            throw new RecordAlreadyExistsException("Email '" + body.email() + "' is already in use");
+        if (userRepository.existsByUsername(body.username()))
+            throw new RecordAlreadyExistsException("Username '" + body.username() + "' is already in use");
+
+        String imageUrl;
+
+        if (profileImage.isEmpty()) {
+
+            imageUrl = "https://ui-avatars.com/api/?name=" + body.firstName() + "+" + body.lastName();
+
+        } else {
+
+            if (profileImage.getSize() >= 10485760)
+                throw new FileNotAllowedException("File size can't be more than 10MB");
+            if (!(Objects.equals(profileImage.getContentType(), "image/jpeg") || Objects.equals(profileImage.getContentType(), "image/gif") || Objects.equals(profileImage.getContentType(), "image/png") || Objects.equals(profileImage.getContentType(), "image/webp")))
+                throw new FileNotAllowedException("File must be an img");
+
+
+            try {
+                Map result = fileUploader.uploader().upload(profileImage.getBytes(), ObjectUtils.emptyMap());
+                imageUrl = (String) result.get("secure_url");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        return userRepository.save(new User(body.username(), body.email(), bcrypt.encode(body.password()), body.firstName(), body.lastName(), imageUrl));
+
+    }
+}
