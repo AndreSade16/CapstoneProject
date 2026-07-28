87fd31e69c02bd3a0353ebc6b84e2cd1fa96fc1b
Andrea Saderi <andrea.saderi16@gmail.com>
2026-07-28 17:28:41 +0200

    adds exceptions

---
+++ src/main/java/andreasaderi/capstone/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package andreasaderi.capstone.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
